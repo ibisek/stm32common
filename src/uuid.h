172e3cac73b5c688cc5cab32e80014797f532373
ibisek <ibisek@protonmail.com>
2020-12-25 20:39:09 +0100

    L1xx support

--- src/uuid.h
+++ src/uuid.h
@@ -12,7 +12,7 @@
 #define __UUID_H
 
 /**
- * The STM32 factory-programmed UUID memory.
+ * The STM32 factory-programmed UUID memory (see ref. manual, search for Unique device ID register)
  * Three values of 32 bits each starting at this address
  * Use like this: STM32_UUID[0], STM32_UUID[1], STM32_UUID[2]
  */
@@ -22,9 +22,16 @@
 #ifdef STM32F10x
 #define STM32_UUID ((uint32_t *)0x1FFFF7E8)	// address for STM32F10x
 #else
+#ifdef STM32L1
+// Base address: 0x1FF80050 for Cat.1 and Cat.2 devices and 0x1FF800D0 for Cat.3, Cat.4, Cat.5 and Cat.6 devices
+// For device category see section 1.4 of the reference manual.
+#define STM32_UUID ((uint32_t *)0x1FF80050)	// address for STM32L1xx
+#else
 #error "Yet unsupported architecture"
 #endif
 #endif
+#endif
+// F40x 0x1FFF7A10
 
 void printCpuId(void) {
 	serial_print("CPU id: ");
