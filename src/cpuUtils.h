2dcf32247d95da89c051d12197d5010067d62492
ibisek <ibisek@protonmail.com>
2021-01-13 09:33:28 +0100

    finally working init32MHzClock() for L152! :)

--- src/cpuUtils.h
+++ src/cpuUtils.h
@@ -21,6 +21,8 @@
 #ifdef STM32L1
 #include <stm32l1xx_flash.h>
 #include <stm32l1xx_rcc.h>
+#include <stm32l1xx_flash.h>
+#include <stm32l1xx_pwr.h>
 #include <system_stm32l1xx.h>
 #else
 #error "Yet unsupported architecture"
@@ -94,6 +96,7 @@ void init24MHzClock() {
 	SystemCoreClockUpdate();
 }
 
+#if defined(STM32F030xC) || defined(STM32F042) || defined(STM32F10x)
 void init32MHzClock() {
 	FLASH_SetLatency (FLASH_Latency_1);
 
@@ -105,12 +108,8 @@ void init32MHzClock() {
 	// Set up 32 MHz Core Clock using HSI (8Mhz/2) x 8 = 32MHz
 	RCC_PLLConfig(RCC_PLLSource_HSI_Div2, RCC_PLLMul_8);
 	#endif
-	#ifdef STM32L1
-	// Set up 32 MHz Core Clock using HSI (16Mhz/2) x 4 = 32MHz
-	RCC_PLLConfig(RCC_PLLSource_HSI, RCC_PLLMul_4, RCC_PLLDiv_2);
-	#endif
 
-	RCC_PLLCmd (ENABLE);
+	RCC_PLLCmd(ENABLE);
 
 	// Wait for PLLRDY after enabling PLL.
 	while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) != SET) {
@@ -119,6 +118,35 @@ void init32MHzClock() {
 	RCC_SYSCLKConfig (RCC_SYSCLKSource_PLLCLK);  // Select the PLL as clock source.
 	SystemCoreClockUpdate();
 }
+#endif
+#ifdef STM32L1
+void init32MHzClock() {
+	FLASH_PrefetchBufferCmd(ENABLE);
+	FLASH_ReadAccess64Cmd(ENABLE);
+	FLASH_SetLatency(FLASH_Latency_1);
+	PWR_VoltageScalingConfig(PWR_VoltageScaling_Range1);
+
+	RCC_HCLKConfig(RCC_SYSCLK_Div1);	// HCLK = SYSCLK
+	RCC_PCLK2Config(RCC_HCLK_Div1); 	// PCLK2 = HCLK
+	RCC_PCLK1Config(RCC_HCLK_Div1); 	// PCLK1 = HCLK
+
+	RCC_AdjustHSICalibrationValue(0x10U);	// default HSI calibration trimming value
+	RCC_HSICmd(ENABLE);
+	while (RCC_GetFlagStatus(RCC_FLAG_HSIRDY) != SET);
+
+	// PLL clock from 16 MHz HSI:
+	RCC_PLLConfig(RCC_PLLSource_HSI, RCC_PLLMul_4, RCC_PLLDiv_2);	// 16*4/2 = 32MHz
+
+	RCC_PLLCmd(ENABLE);
+	while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) != SET);
+
+	RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);  // select the PLL as clock source.
+	while(RCC_GetSYSCLKSource() != 0x0C);  		// wait till PLL is used as system clock
+
+	SystemCoreClock = 32000000;
+}
+
+#endif
 
 #ifdef STM32F10x
 void init36MHzClock() {
